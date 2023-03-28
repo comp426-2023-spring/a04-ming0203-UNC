674e3c670b9415c97fbfa258856664bda04a3e34
ming0203-UNC <ming0203@email.unc.edu>
2023-03-28 18:12:24 -0400

    edit server.js add rps body.shot and param.shot

--- server.js
+++ server.js
@@ -29,8 +29,16 @@ app.get('/app/rpsls/play/', (req,res)=>{
     res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
+app.get('/app/rps/play/', (req,res)=>{
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
 app.get('/app/rpsls/play/', (req,res)=>{
     res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
+
+app.get('/app/rps/play/:shot', (req,res)=>{
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
 })
 
 app.get('/app/rpsls/play/:shot', (req,res)=>{
